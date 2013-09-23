1381995b5c9cf5c2d573f65475b4932488bbfd56
Javier Uruen Val <javi.uruen@gmail.com>
2013-09-23 16:59:35 +0200

    Add client

---
+++ extra/client.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import bernhard
+import datetime
+import sys
+import random
+
+batch_size = 10
+services = ["test1," "test2", "test3", "foo", "bar", "baz", "xyzzy", "attack", "cat", "treat"]
+
+class BatchClient(bernhard.Client):
+    def send(self, events):
+          message = bernhard.Message(events=[bernhard.Event(params=e) for e in events])
+          response = self.transmit(message)
+          return response.ok
+
+def create_event(i):
+    global services
+    return {
+        'host': "host%i" %i ,
+        'service': services[i % len(services)],
+        'metric': random.random(),
+        'description': str(datetime.datetime.now()),
+        'tags': ["stress-test"],
+        'attributes' : { 'foo' : 'bar'}
+    }
+
+c = BatchClient(host='localhost')
+
+#while True:
+c.send([create_event(i) for i in range(batch_size)])
