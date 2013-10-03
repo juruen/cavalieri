c0e12fb9027f54dc04792d82b4e3bf64f5741908
Javier Uruen Val <javi.uruen@gmail.com>
2013-10-04 00:18:54 +0200

    Add more stream functionality

--- src/streams.cpp
+++ src/streams.cpp
@@ -11,14 +11,19 @@ static inline std::string metric_to_string(const Event& e) {
   } else if (e.has_metric_sint64()) {
     ss << e.metric_sint64();
   } else {
-    ss << "nil";
+    ss << "";
   }
   return ss.str();
 }
 
-stream_t Streams::prn() {
+void call_rescue(const Event& e, const children_t& children) {
+  for (auto& s: children) {
+    s(e);
+  }
+}
+
+stream_t prn() {
   return [](const Event& e) {
-    return;
     VLOG(3) << "prn()";
     LOG(INFO) << "prn() { host: '" <<  e.host() << "' service: '" << e.service()
               << "' description: '" << e.description()
@@ -27,10 +32,40 @@ stream_t Streams::prn() {
   };
 }
 
+stream_t with(const with_changes_t& changes, const children_t& children) {
+  return [=](const Event& e) {
+    Event ne(e);
+    for (auto &kv: changes) {
+      VLOG(3) << "with()  first: " << kv.first << " second: " << kv.second;
+      std::string key = kv.first;
+      if (key == "host") {
+        ne.set_host(kv.second);
+      } else if (key == "service") {
+        ne.set_service(kv.second);
+      } else if (key == "description") {
+        ne.set_description(kv.second);
+      } else if (key == "state") {
+        ne.set_state(kv.second);
+      } else if (key == "metric") {
+        if (e.has_metric_f()) {
+          ne.set_metric_f(atof(kv.second.c_str()));
+        } else if (e.has_metric_d()) {
+          ne.set_metric_d(atof(kv.second.c_str()));
+        } else if (e.has_metric_sint64()) {
+          ne.set_metric_sint64(atoi(kv.second.c_str()));
+        }
+      }
+    }
+    call_rescue(ne, children);
+  };
+}
+
 void Streams::add_stream(stream_t stream) {
   VLOG(3) << "adding stream";
   streams.push_back(stream);
 }
+
+
 
 void Streams::process_message(const Msg& message) {
   VLOG(3) << "process message. num of streams " << streams.size();
--- src/streams.h
+++ src/streams.h
@@ -6,13 +6,19 @@
 #include <proto.pb.h>
 
 typedef std::function<void(const Event&)> stream_t;
-typedef std::list<stream_t> childrent_t;
+typedef std::list<stream_t> children_t;
+typedef std::map<std::string, std::string> with_changes_t;
+
+
+void call_rescue(const Event& e, const children_t& children);
+
+stream_t prn();
+stream_t with(const with_changes_t& changes, const children_t& children);
 
 struct Streams {
   std::list<stream_t> streams;
   void add_stream(stream_t stream);
   void process_message(const Msg& message);
-  static stream_t prn();
 };
 
 #endif
