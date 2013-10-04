0136db3a3e15ff67ec6d6c9b856d79c2e678225c
Javier Uruen Val <javi.uruen@gmail.com>
2013-10-04 09:52:52 +0200

    Add e_t

--- src/streams.h
+++ src/streams.h
@@ -5,15 +5,17 @@
 #include <functional>
 #include <proto.pb.h>
 
-#define PRED(EXP) [](const Event& e) { return (EXP); }
+typedef const Event& e_t;
+
+#define PRED(EXP) [](e_t e) { return (EXP); }
 #define CHILD(EXP) {EXP}
 
-typedef std::function<void(const Event&)> stream_t;
-typedef std::function<bool(const Event&)> predicate_t;
+typedef std::function<void(e_t)> stream_t;
+typedef std::function<bool(e_t)> predicate_t;
 typedef std::list<stream_t> children_t;
 typedef std::map<std::string, std::string> with_changes_t;
 
-void call_rescue(const Event& e, const children_t& children);
+void call_rescue(e_t e, const children_t& children);
 
 stream_t prn();
 stream_t with(const with_changes_t& changes, const children_t& children);
