b19aa32457d31e3586e0cbe1baedf4b6508cc19d
Javier Uruen Val <javi.uruen@gmail.com>
2013-10-05 12:22:15 +0200

    Implement split function

--- src/streams.h
+++ src/streams.h
@@ -13,6 +13,7 @@ typedef const Event& e_t;
 typedef std::function<void(e_t)> stream_t;
 typedef std::function<bool(e_t)> predicate_t;
 typedef std::list<stream_t> children_t;
+typedef std::pair<const predicate_t, const children_t> split_pair_t;
 typedef std::map<std::string, std::string> with_changes_t;
 
 void call_rescue(e_t e, const children_t& children);
@@ -24,7 +25,7 @@ stream_t with(const with_changes_t& changes, const children_t& children);
 stream_t where(const predicate_t& predicate, const children_t& children,
                const children_t& else_children={});
 
-stream_t split(std::list<const predicate_t&, const children_t&> clauses,
+stream_t split(std::list<split_pair_t> clauses,
                const children_t& default_children={});
 
 stream_t rate(const int seconds, const children_t& children);
