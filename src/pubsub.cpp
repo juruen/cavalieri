87c25c1cda5d7aab53ac7305e824e5ffb973ba93
Javier Uruen Val <javi.uruen@gmail.com>
2013-10-10 22:32:55 +0200

    Add and use unsubscribe to pubsub and weboscket

--- src/pubsub.cpp
+++ src/pubsub.cpp
@@ -19,25 +19,54 @@ void PubSub::publish(const std::string& topic, const std::string eventstr) {
   }
 }
 
-void PubSub::subscribe(const std::string& topic, const notify_f_t notify_f) {
+void PubSub::subscribe(
+    const std::string& topic,
+    const notify_f_t notify_f,
+    const uintptr_t id)
+{
   VLOG(3) << "subscribe() topic: " << topic;
-  auto it_sub = subscribers.find(topic);
-  if (it_sub == subscribers.end()) {
-    VLOG(3) << "subscribe() first subscriber";
-    subscribers.insert({topic, {notify_f}});
-  } else {
-    VLOG(3) << "subscribe() adding subscriber to list";
-    it_sub->second.push_back(notify_f);
+  auto sub_it = subscribers.find(topic);
+  if (sub_it == subscribers.end()) {
+    VLOG(3) << "subscribe() first subscriber to topic, creating list";
+    sub_it = subscribers.insert(sub_it, {topic, {}});
   }
+  VLOG(3) << "subscribe() adding subscriber " << id << " to existing list";
+  sub_it->second.push_back(notify_f);
 
-  auto it_pub = publishers.find(topic);
-  if (it_pub == publishers.end()) {
+  subscribers_iterators.insert({id, --(sub_it->second.end())});
+
+  auto pub_it = publishers.find(topic);
+  if (pub_it == publishers.end()) {
     VLOG(3) << "subscribe() not publisher for this topic yet";
     return;
   }
-  auto f = it_pub->second;
+  auto f = pub_it->second;
   VLOG(3) << "subscribe() fetching all events";
   const evstr_list_t evs = f();
   VLOG(3) << "subscribe() sending events";
   notify_f(evs, "");
+}
+
+void PubSub::unsubscribe(const std::string& topic, const uintptr_t id) {
+  VLOG(3) << "unsubscribe() id: " << id << " from topic: " << topic;
+
+  auto sub_it_it = subscribers_iterators.find(id);
+  if (sub_it_it == subscribers_iterators.end()) {
+    VLOG(3) << "id doesn't exist";
+    return;
+  }
+
+  auto sub_it = subscribers.find(topic);
+  if (sub_it == subscribers.end())  {
+    VLOG(3) << "topic doesn't exist";
+    return;
+  }
+
+  VLOG(3) << "using iterator to unsubscribe";
+  sub_it->second.erase(sub_it_it->second);
+
+  if (sub_it->second.size() == 0) {
+    VLOG(3) << "subscriber list empty, removing topic";
+    subscribers.erase(sub_it);
+  }
 }
--- src/pubsub.h
+++ src/pubsub.h
@@ -11,17 +11,21 @@ typedef std::list<std::string> evstr_list_t;
 typedef std::function<evstr_list_t()> allevents_f_t;
 typedef std::unordered_map<std::string, allevents_f_t> publishers_t;
 typedef std::function<void(const evstr_list_t&, const std::string)> notify_f_t;
-typedef std::unordered_map<std::string, std::list<notify_f_t>> subscribers_t;
+typedef std::list<notify_f_t> notify_f_list_t;
+typedef std::unordered_map<std::string, notify_f_list_t> subscribers_t;
+typedef std::unordered_map<uintptr_t, notify_f_list_t::iterator> subscribers_iterators_t;
 
 class PubSub {
 private:
   publishers_t publishers;
   subscribers_t subscribers;
+  subscribers_iterators_t subscribers_iterators;
 
 public:
   void add_publisher(const std::string& topic, const allevents_f_t& allevents_f);
   void publish(const std::string& topic, const std::string eventstr);
-  void subscribe(const std::string& topic, const notify_f_t notify_f);
+  void subscribe(const std::string& topic, const notify_f_t notify_f, const uintptr_t id);
+  void unsubscribe(const std::string& topic, const uintptr_t id);
 };
 
 #endif
