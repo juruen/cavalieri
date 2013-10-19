2344b11ce99e0cbe72e1e14a0ab3942c62768319
Javier Uruen Val <javi.uruen@gmail.com>
2013-10-19 16:59:11 +0200

    Split expression.h in cpp

--- src/expression.h
+++ src/expression.h
@@ -4,18 +4,14 @@
 #include <map>
 #include <vector>
 #include <ostream>
-#include <stdexcept>
-#include <cmath>
 #include <iostream>
 
 class QueryNode
 {
   public:
     virtual void print(std::ostream &os, unsigned int depth=0) const = 0;
-    static inline std::string indent(unsigned int d) {
-      return std::string(d * 2, ' ');
-    }
-    virtual ~QueryNode() {};
+    static inline std::string indent(unsigned int d);
+    virtual ~QueryNode();
 };
 
 class QueryTagged : public QueryNode
@@ -23,12 +19,8 @@ class QueryTagged : public QueryNode
   std::string* string;
 
 public:
-  QueryTagged(std::string* string) : QueryNode(), string(string)
-  {
-  }
-  virtual void print(std::ostream &os, unsigned int depth=0) const {
-    os << indent(depth) << "tagged = " << *string << std::endl;
-  }
+  QueryTagged(std::string* string);
+  virtual void print(std::ostream &os, unsigned int depth=0) const;
 };
 
 class QueryAnd : public QueryNode
@@ -37,18 +29,8 @@ class QueryAnd : public QueryNode
   QueryNode* right;
 
 public:
-  QueryAnd(QueryNode* left, QueryNode* right) :
-    QueryNode(),
-    left(left),
-    right(right)
-  {
-  }
-
-  virtual void print(std::ostream &os, unsigned int depth=0) const {
-    os << indent(depth) << " and" << std::endl;
-    left->print(os, depth + 1);
-    right->print(os, depth + 1);
-  }
+ QueryAnd(QueryNode* left, QueryNode* right);
+ virtual void print(std::ostream &os, unsigned int depth=0) const;
 
 };
 
@@ -58,19 +40,8 @@ class QueryOr : public QueryNode
   QueryNode* right;
 
 public:
-  QueryOr(QueryNode* left, QueryNode* right) :
-    QueryNode(),
-    left(left),
-    right(right)
-  {
-  }
-
-  virtual void print(std::ostream &os, unsigned int depth=0) const {
-    os << indent(depth) << " or" << std::endl;
-    left->print(os, depth + 1);
-    right->print(os, depth + 1);
-  }
-
+  QueryOr(QueryNode* left, QueryNode* right);
+  virtual void print(std::ostream &os, unsigned int depth=0) const;
 };
 
 class QueryContext
@@ -78,38 +49,11 @@ class QueryContext
   public:
 
     typedef std::map<std::string, double> variablemap_type;
-
     variablemap_type		variables;
-
     std::vector<QueryNode*> expressions;
 
-    ~QueryContext()
-    {
-      clearExpressions();
-    }
-
-    void	clearExpressions()
-    {
-      for(unsigned int i = 0; i < expressions.size(); ++i)
-      {
-        delete expressions[i];
-      }
-      expressions.clear();
-    }
-
-    bool	existsVariable(const std::string &varname) const
-    {
-      return variables.find(varname) != variables.end();
-    }
-
-    double	getVariable(const std::string &varname) const
-    {
-      variablemap_type::const_iterator vi = variables.find(varname);
-      if (vi == variables.end())
-        throw(std::runtime_error("Unknown variable."));
-      else
-        return vi->second;
-    }
+    ~QueryContext();
+    void	clearExpressions();
 };
 
-#endif 
+#endif
