aa366a4ee990b6ac85e79e3057c6db043dc3fc86
Javier Uruen Val <javi.uruen@gmail.com>
2013-10-19 18:10:52 +0200

    Return function that evaluates an AST of a query

--- src/expression.cpp
+++ src/expression.cpp
@@ -1,4 +1,6 @@
-#include "expression.h"
+#include <expression.h>
+#include <util.h>
+#include <glog/logging.h>
 
 inline std::string QueryNode::indent(unsigned int d) {
   return std::string(d * 2, ' ');
@@ -14,6 +16,14 @@ void QueryTagged::print(std::ostream &os, unsigned int depth) const {
   os << indent(depth) << "tagged = " << *string << std::endl;
 }
 
+query_f_t QueryTagged::evaluate() const {
+  std::string tag(string->substr(1, string->size() - 2));
+  return [=](const Event& e) {
+    VLOG(3) << "QueryTagged()::evaluate() tag: " << tag;
+    return tag_exists(e, tag);
+  };
+}
+
 QueryAnd::QueryAnd(QueryNode* left, QueryNode* right) :
   QueryNode(),
   left(left),
@@ -27,6 +37,14 @@ void QueryAnd::print(std::ostream &os, unsigned int depth) const {
   right->print(os, depth + 1);
 }
 
+query_f_t QueryAnd::evaluate() const {
+  query_f_t left_f = left->evaluate();
+  query_f_t right_f = right->evaluate();
+  return [=](const Event& e) {
+    VLOG(3) << "QueryAnd()::evaluate()";
+    return (left_f(e) && right_f(e));
+  };
+}
 
 QueryOr::QueryOr(QueryNode* left, QueryNode* right) :
   QueryNode(),
@@ -41,6 +59,14 @@ void QueryOr::print(std::ostream &os, unsigned int depth) const {
   right->print(os, depth + 1);
 }
 
+query_f_t QueryOr::evaluate() const {
+  query_f_t left_f = left->evaluate();
+  query_f_t right_f = right->evaluate();
+  return [=](const Event& e) {
+    VLOG(3) << "QueryOr()::evaluate()";
+    return (left_f(e) || right_f(e));
+  };
+}
 
 QueryContext::~QueryContext()
 {
