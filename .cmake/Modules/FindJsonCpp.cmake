c1cf1a2a53d6e49e3b3ef7f4c33755de0ea92922
Javier Uruen Val <javi.uruen@gmail.com>
2014-01-27 18:48:50 +0100

    Add jsoncpp dependency

---
+++ .cmake/Modules/FindJsonCpp.cmake
@@ -0,0 +1,17 @@
+include(LibFindMacros)
+
+libfind_pkg_check_modules(JsonCpp_PKGCONF jsoncpp)
+
+find_path(JsonCpp_INCLUDE_DIR
+    NAMES json/features.h
+    PATHS ${JsonCpp_PKGCONF_INCLUDE_DIRS} # /usr/include/jsoncpp/json
+  )
+
+find_library(JsonCpp_LIBRARY
+    NAMES jsoncpp
+    PATHS ${JsonCpp_PKGCONF_LIBRARY_DIRS}
+  )
+
+set(JsonCpp_PROCESS_INCLUDES JsonCpp_INCLUDE_DIR)
+set(JsonCpp_PROCESS_LIBS JsonCpp_LIBRARY)
+libfind_process(JsonCpp)
