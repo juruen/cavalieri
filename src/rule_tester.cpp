aa225654b748db897a69db4903c2fd467b7050a9
Javier Uruen Val <javi.uruen@gmail.com>
2014-01-30 11:47:34 +0100

    Initialize libcds in rules_tester

--- src/rule_tester.cpp
+++ src/rule_tester.cpp
@@ -7,6 +7,7 @@
 #include <rule_tester_util.h>
 #include <rules/rules.h>
 #include <mock_index.h>
+#include "atom.h"
 
 DEFINE_string(input_events, "", "json string containing input events");
 
@@ -15,26 +16,34 @@ scheduler g_scheduler{mock_sched};
 external_mocks g_external_mocks{};
 
 int main(int argc, char **argv) {
-  google::ParseCommandLineFlags(&argc, &argv, true);
+  cds::Initialize();
 
-  bool ok;
-  std::vector<Event> events = json_to_events(FLAGS_input_events, ok);
+  {
+    cds::gc::HP hpGC;
+    atom<bool>::attach_thread();
 
-  if (!ok) {
-    LOG(ERROR) << "couldn't parse input events";
-    return -1;
+    google::ParseCommandLineFlags(&argc, &argv, true);
+
+    bool ok;
+    std::vector<Event> events = json_to_events(FLAGS_input_events, ok);
+
+    if (!ok) {
+      LOG(ERROR) << "couldn't parse input events";
+      return -1;
+    }
+
+    mock_index idx;
+    class index index(idx);
+    auto rule_stream = sdo(rules(index));
+
+    for (const auto & event: events) {
+      mock_sched.process_event_time(event.time());
+      rule_stream(event);
+    }
+
+    std::cout << results(idx.events(), g_external_mocks.calls()) << "\n";
   }
-
-  mock_index idx;
-  class index index(idx);
-  auto rule_stream = rules(index);
-
-  for (const auto & event: events) {
-    mock_sched.process_event_time(event.time());
-    rule_stream(event);
-  }
-
-  std::cout << results(idx.events(), g_external_mocks.calls()) << "\n";
+  cds::Terminate();
 
   return 0;
 }
