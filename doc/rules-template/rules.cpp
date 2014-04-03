64c44267a7d74e863f0581c27a0cc5f07e88589e
Javier Uruen Val <javi.uruen@gmail.com>
2014-04-03 22:57:06 +0200

    Move rules-template to its own repo

--- doc/rules-template/rules.cpp
+++
@@ -1,8 +0,0 @@
-#include <rules.h>
-#include <external/pagerduty.h>
-#include <external/email.h>
-
-streams_t* rules() {
-//  return new streams_t(email("localhost", "juruen@foobar", "juruen@fooops"));
-  return new streams_t(prn());
-}
